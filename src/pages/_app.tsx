71a9abecc2dce50f52c49134794428f0625d3fdd
임성호 <sungho.lim1@wesang.com>
2023-12-26 21:14:02 +0900

    feat: add tanstack query devtools component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
 import { useState } from "react";
 
@@ -26,6 +27,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <HydrationBoundary state={pageProps.dehydratedState}>
         <Component {...pageProps} />
       </HydrationBoundary>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
