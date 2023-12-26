4898e8f70562cd6f06117a7ae200a57114f09498
임성호 <sungho.lim1@wesang.com>
2023-12-26 21:13:45 +0900

    feat: add transpilePackages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  transpilePackages: [
+    "@tanstack/react-query",
+    "@tanstack/react-query-devtools",
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
